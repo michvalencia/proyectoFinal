327b75c5a077394e97787e98c7615d9f31fbaaa6
michvalencia <michcaicedo.26@gmail.com>
2025-06-02 11:45:11 -0500

    analiza estado del estudiante

--- proyecto.js
+++ proyecto.js
@@ -22,9 +22,9 @@ for (i = 0 ; i < cantEstudiantes ; i++){
     let nombre = prompt(`Ingrese el nombre del estudiante #${i+1}:   `)
     let codigo = parseInt(prompt(`Ingrese el codigo del estudiante #${i+1}:   `))
     let grupo = prompt(`Ingrese el grupo del estudiante #${i+1}:    `)
-    let nota1 = parseInt(prompt(`Ingrese la nota 1 del estudiante #${i+1}:   `))
-    let nota2 = parseInt(prompt(`Ingrese la nota 2 del estudiante #${i+1}:   `))
-    let nota3 = parseInt(prompt(`Ingrese la nota 3 del estudiante #${i+1}:   `))
+    let nota1 = parseFloat(prompt(`Ingrese la nota 1 del estudiante #${i+1}:   `))
+    let nota2 = parseFloat(prompt(`Ingrese la nota 2 del estudiante #${i+1}:   `))
+    let nota3 = parseFloat(prompt(`Ingrese la nota 3 del estudiante #${i+1}:   `))
     let asistencia = parseInt(prompt(`Ingrese el porcentaje de asistencia del estudiante #${i+1}:   `))
     let entregas = parseInt(prompt(`Ingrese el porcentaje de entregas del estudiante #${i+1}:    `))
 
@@ -32,7 +32,17 @@ for (i = 0 ; i < cantEstudiantes ; i++){
 
     let promedioNotas = (nota1+nota2+nota3)/3
 
+    if(entregas < 30){
+         estado = "critico"
+    }else if(promedioNotas >= 4 && asistencia >= 80){
+        estado = "excelente"
+    } else if(promedioNotas >= 3 && promedioNotas < 4 && asistencia >= 60 && asistencia < 80){
+        estado = "aceptable"
+    }  else {
+        estado = "critico"
+    }
+
     estudiantes[i] = new Est(nombre,codigo,grupo,nota1,nota2,nota3,asistencia,entregas,promedioNotas,estado)
 }
-console.log(estudiantes[0]);
+console.log(estudiantes);
 
