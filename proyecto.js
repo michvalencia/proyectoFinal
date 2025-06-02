aacacbe93bcc6298258097a3473adb8b87527cc4
michvalencia <michcaicedo.26@gmail.com>
2025-06-02 11:40:10 -0500

    añadir estudiantes y sus atributos

--- proyecto.js
+++ proyecto.js
@@ -1,2 +1,38 @@
 var prompt = require('prompt-sync')()
 
+class Est {
+    constructor(nombre,codigo,grupo,nota1,nota2,nota3,asistencia,entregas,promedioNotas,estado) {
+        this.nombre = nombre
+        this.codigo = codigo
+        this.grupo = grupo
+        this.nota1 = nota1
+        this.nota2 = nota2
+        this.nota3 = nota3
+        this.asistencia = asistencia
+        this.entregas = entregas
+        this.promedioNotas = promedioNotas
+        this.estado = estado
+    }
+}
+
+let cantEstudiantes = parseInt(prompt("Ingrese la cantidad de estudiantes a ingresar:    "))
+let estudiantes = []
+
+for (i = 0 ; i < cantEstudiantes ; i++){
+    let nombre = prompt(`Ingrese el nombre del estudiante #${i+1}:   `)
+    let codigo = parseInt(prompt(`Ingrese el codigo del estudiante #${i+1}:   `))
+    let grupo = prompt(`Ingrese el grupo del estudiante #${i+1}:    `)
+    let nota1 = parseInt(prompt(`Ingrese la nota 1 del estudiante #${i+1}:   `))
+    let nota2 = parseInt(prompt(`Ingrese la nota 2 del estudiante #${i+1}:   `))
+    let nota3 = parseInt(prompt(`Ingrese la nota 3 del estudiante #${i+1}:   `))
+    let asistencia = parseInt(prompt(`Ingrese el porcentaje de asistencia del estudiante #${i+1}:   `))
+    let entregas = parseInt(prompt(`Ingrese el porcentaje de entregas del estudiante #${i+1}:    `))
+
+    let estado = ""
+
+    let promedioNotas = (nota1+nota2+nota3)/3
+
+    estudiantes[i] = new Est(nombre,codigo,grupo,nota1,nota2,nota3,asistencia,entregas,promedioNotas,estado)
+}
+console.log(estudiantes[0]);
+
